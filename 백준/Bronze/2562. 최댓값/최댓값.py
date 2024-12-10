1176df5eeaef8436d67d3772602930d92ec72d4c
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-12-10 14:22:32 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,13 +1,20 @@
+import sys
+
+#최대값을 구하기 위한 전역변수 설정
 num_list = []
+max = -1
+max_idx = -1
 
-for i in range(0,9):
-    num = int(input())
+#배열에 추가된 숫자 중 가장 큰 수를 max, max_idx 변수에 업데이트한다.
+for i in range(9):
+    num = int(sys.stdin.readline())
     num_list.append(num)
-    
-print(max(num_list))
-print(num_list.index(max(num_list))+1)
 
-                    
+    if num_list[i] > max:
+        max = num_list[i]
+        max_idx = i+1
+
+print(max)
+print(max_idx)
 
 
-        
