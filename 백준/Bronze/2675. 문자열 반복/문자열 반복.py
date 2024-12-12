68c9fe30f3e378d4fb7680b2f59851fbeaf35f9f
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-12-13 02:11:30 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,14 +1,14 @@
 import sys
 read = sys.stdin.readline
-
-n = int(read().strip())
+n = int(read())
 
 for i in range(n):
-    rpt_list = read().strip().split()
-    rpt, rptstr = int(rpt_list[0]), rpt_list[1]
+    result = ""
     
-    for i in range(len(rptstr)):
-        print(rptstr[i] * rpt, end = '')
-    print()
-
-      
+    testcase = read().strip().split()
+    num = int(testcase[0])
+    test = testcase[1]
+    
+    for j in range(len(test)):
+        result += test[j] * num
+    print(result)
