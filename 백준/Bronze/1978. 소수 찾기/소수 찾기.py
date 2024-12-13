363c1eccc957858ea005e89aabb2c1a9d6b3d375
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-12-13 23:33:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,26 +1,18 @@
-import math 
+import sys
+read = sys.stdin.readline
 
-tot_num = int(input())
-tot_num_list = list(map(int,input().split()))
+n = int(read())
+testcase = list(map(int,read().strip().split()))
 
-tot_count = 0
-count = 0
-
-for i in range(tot_num):
-    num = tot_num_list[i]
-    
-    if num < 2 :
+tot = 0
+for i in range(n):
+    cnt =0
+    if testcase[i]<2:
         continue
-    
-    is_prime = True
-    for k in range(2, int(math.sqrt(num)) +1):
-        if num % k == 0:
-            is_prime = False
+    for j in range(2, int(testcase[i]**0.5)+1):
+        if testcase[i]%j==0:
+            cnt+=1
             break
-            
-    if is_prime:
-        count+=1
-        
-tot_count+=count
-
-print(tot_count)        
+    if cnt == 0:
+        tot+=1
+print(tot)
