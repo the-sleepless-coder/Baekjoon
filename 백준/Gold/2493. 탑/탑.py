0d54fe0889f2bfe5e93af7a27b9c97a1a168592c
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2025-01-19 23:15:23 +0900

    [Gold V] Title: 탑, Time: 612 ms, Memory: 166164 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,25 +1,23 @@
 import sys
-
-read = sys.stdin.readline
-
-n = int(read().strip())
-bld_list = list(map(int,read().strip().split()))
-
+read = lambda: sys.stdin.readline().strip()
+    
+n = int(read())
+building_list = list(map(int,read().split()))
 stack = []
 result = []
+
 for i in range(n):
-    while stack and stack[-1][0] < bld_list[i]:
+    while stack and stack [-1][1] < building_list[i]:
         stack.pop()
-
-    if stack:
-        # print(stack[-1][1]+1)
-        result.append(stack[-1][1]+1)
     
+    if stack:
+        result.append(stack[-1][0] + 1)
     else:
-        # print(0)
         result.append(0)
+    
+    stack.append([i, building_list[i]])
 
-    stack.append([bld_list[i], i])
+print(' '.join(map(str, result)))
 
-for idx in result:
-    print(idx, end = ' ')
+    
+    
