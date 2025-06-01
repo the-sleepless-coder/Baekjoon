d4093de3fe95341f2872ba64fa813d0b76cf8c01
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2025-06-01 19:50:01 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 936 ms, Memory: 296476 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,89 +1,55 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] stringInput = input.split(" ");
-        int len = stringInput.length;
-        int[] intInput = new int[len];
-        for(int i=0; i<len; i++){
-            intInput[i] = Integer.parseInt(stringInput[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[2];
+        for(int i = 0; i < 2; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int pplNum = intInput[0];
-        int spaceNum = intInput[1];
-        // 정적 배열을 이용할 시, 죽은 사람을 빼서 배열을 관리하기 어렵기 때문에 동적 배열을 써야한다.
-//        int[] josephus = new int[pplNum];
-//
-//        for(int i=1; i<=pplNum; i++){
-//            josephus[i-1] = i;
-//        }
-//
-//        int idx = (spaceNum-1);
-//
-//        while(josephus[idx]!=0){
-//            System.out.println(idx+1);
-//            System.out.println(josephus[idx]);
-//            josephus[idx]=0;
-//            pplNum -=1 ;
-//            idx= (-1+idx+spaceNum) % pplNum;
-//        }
-//
-//
-//        System.out.println(Arrays.toString(josephus));
+        int tot = arr[0];
+        int n = arr[1];
 
-        // 요세푸스 순열을 관리하기 위한 배열을 만든다.
-        // 동적 배열을 이용해야, 한명씩 죽어나갈 때 해당 사람을 뺄 수 있다.
-        List<Integer> josephus = new ArrayList<>();
-
-        for(int i=1; i<=pplNum; i++){
-            josephus.add(i);
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =1; i <= tot; i++){
+            q.add(i);
         }
 
-        int idx = (spaceNum-1);
+        // System.out.println(q);
 
-        System.out.printf("<");
-        while(!josephus.isEmpty()){
-            // 마지막 원소에 도달할 때 출력문을 다르게 구성하기 위한 조건문을 구성한다.
-            if(josephus.size()!=1){
-                System.out.printf("%d, ",josephus.get(idx));
-            }else{
-                System.out.printf("%d",josephus.get(idx));
+        while(!q.isEmpty()){
+            for(int i = 0; i < n - 1; i++){
+                int temp = q.poll();
+                q.add(temp);
             }
-            josephus.remove(idx);
-            pplNum -=1;
-            // System.out.println(pplNum);
-            // 마지막에 josephus 순열이 비어 있지 않는데도, idx를 업데이트하는 것을 방지하기 위한 코드
-            if(pplNum != 0){
-                idx = (idx + spaceNum - 1) % pplNum;
-            }
+            int ext = q.poll();
+            list.add(ext);
         }
-        System.out.printf(">");
 
-//        System.out.println(josephus);
+        // System.out.println(list);
 
+        for(int i=0; i<list.size(); i++){
+            if(i==0){
+                System.out.print("<");
+            }
+            
+            System.out.printf("%d",list.get(i));
+            if(i!=list.size()-1){
+                System.out.print(", ");
+            }
 
+            if(i==list.size()-1){
+                System.out.print(">");
+            }
 
-
-
-        // Queue는 LinkedList를 이용해서 구현한다.
-//        Queue<Integer> q = new LinkedList<>();
-
-//        for(int i=1; i<=pplNum; i++){
-//            q.add(i);
-//        }
-//
-//        for(int i=0 ; i< pplNum; i++){
-//            q.remove();
-//
-//        }
-//
-//
-//        System.out.println(q);
+        }
 
 
     }
