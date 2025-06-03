640b305eed4a43cd69df779623ccdb2ce3a3db36
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2025-06-03 11:04:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 13428 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,130 +1,120 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        // M*N
-        // 8*8로 만드려고 한다.
-        // 10 * 13일 경우 검증을 할 수 있는 칸의 개수는
-        // (2+1) * (5+1)
-        // (0, 1, 2) * (0, 1, 2, 3, 4, 5)
+    // 왼쪽 위가 W인 경우 바꿔야되는 체스판의 개수
+    // 왼쪽 위가 B인 경우 바꿔야되는 체스 판의 개수
+    // 중에서 작은 것을 고르면 되는 문제이다.
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 
-        // 첫번째 칸이 W인 경우와 B인 경우를 나눠서 확인하면 된다.
-        // 홀수 행, 짝수 행에 따른 판별을 진행하면 된다.
-        // 첫번째 칸에서 짝수 행 짝수 번째 칸만큼 떨어진 곳은, 첫번째 칸과 같아야 한다.
-        // 첫번째 칸에서 홀수 행 홀수 번째 칸만큼 떨어진 곳은, 첫번째 칸과 같아야 한다.
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-        sc.nextLine();
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        char[][] charArr = new char[r][c];
-        for(int i=0; i<r; i++){
-            String temp = sc.nextLine();
-            char[] tempChar = temp.toCharArray();
-            charArr[i] = tempChar;
+        char[][] chess = new char[r][c];
+        for(int i=0; i < r; i++){
+            String row = br.readLine();
+            char[] charArray = row.toCharArray();
+            chess[i] = charArray;
         }
 
-        // System.out.println(Arrays.deepToString(charArr));
+        // 시작점의 체스판 색깔에 따라서,
+        // 짝수 줄과 홀수 줄에 대한 판별을 각각 다르게 해준다.
+        // 세로 기준으로 판별하기는 했으나,
+        // 어차피 첫 체스판 색깔 기준으로 나머지 체스판의 색깔 판별 시 반대로 된다면,
+        // 더 쉬운 방법은 없을까 싶다.
+        int result1 = Integer.MAX_VALUE;
+        int result2 = Integer.MAX_VALUE;
+        int size = 8;
 
-        // 검증 범위 내 각 시작점을 확인한다.
-        int checkR = r - 8 ;
-        int checkC = c - 8 ;
-        int min = Integer.MAX_VALUE;
-        // System.out.println(checkR);
-        // System.out.println(checkC);
+        for(int row=0; row <= c - size; row++){
+            for(int col=0; col <= r - size; col++){
 
-        for(int i = 0; i <= checkR; i++){
-            for(int j = 0; j <= checkC; j++) {
-                // 시작점에서 각 행열로 떨어진만큼의 행열에 어떤 값이 있는지를 확인하면 된다.
-                // 행 열 처리를 모두 해줘야 제대로 개수를 처리할 수 있다.
-                //charArr에 대한 경우를 나눠준다.
-
-                // System.out.printf("%d %d\n", i, j);
-
-                // 시작 위치에서 'B', 'W' 모든 경우에 대한 케이스를 분석해주고, 그 중에서 체스판을 바꿔야 하는 경우의 수가 더 작은 것을 구한다.
-                char[] charCase = {'B','W'};
-                for(char chr: charCase){
-                    // 각 시작 지점에서 changeCol은 초기화 돼야 한다.
-                    // 경우를 나눌 때 초기화 위치에 신경쓰자.
-                    int changeCol = 0;
-
-                    char temp = chr;
-
-                    // 첫번째 칸에서 k,l개의 칸을 떨어진만큼에 대한 처리를 해준다.
-                    for (int k = 0; k < 8 ; k++){
-                        for (int l = 0; l < 8; l++) {
-
-                            if ( k % 2 == 0 ) {
-                                // 짝수 행 짝수 열만큼 떨어진 곳은 같아야 한다.
-                                if(l % 2 == 0){
-                                    if (charArr[i + k][j + l] != temp) {
-                                        changeCol += 1;
-                                        //System.out.print(k);
-                                        //System.out.print(l);
-                                        //System.out.println();
-                                    }
-                                }else{
-                                    // 짝수 행 홀수열만큼 떨어진 곳은 달라야 한다.
-                                    if (charArr[i + k][j + l] == temp) {
-                                        changeCol += 1;
-                                    }
+                int temp= 0;
+                for(int i = row; i < row + size; i++){
+                    for(int j=col; j < col + size; j++){
+                        if(i%2 == 0){
+                            if(j%2==0){
+                                if(chess[j][i]!='W'){
+                                    temp+=1;
                                 }
-
                             }else{
-                                // 홀수 행 홀수 열만큼 떨어진 곳은 같아야 한다.
-                                if(l % 2 == 1){
-                                    if (charArr[i + k][j + l] != temp) {
-                                        changeCol += 1;
-                                    }
-                                }else{
-                                    // 홀수 행 짝수열만큼 떨어진 곳은 달라야 한다.
-                                    if (charArr[i + k][j + l] == temp) {
-                                        changeCol += 1;
-                                    }
+                                if(chess[j][i]!='B'){
+                                    temp+=1;
                                 }
-
-
                             }
 
+                        }else{
+                            if(j%2==0){
+                                if(chess[j][i]!='B'){
+                                    temp+=1;
+                                }
+                            }else{
+                                if(chess[j][i]!='W'){
+                                    temp+=1;
+                                }
+                            }
 
                         }
 
 
                     }
-
-                    // 검증해줘야 하는 곳에서 모두 다 처리했다면, 바꿔야하는 체스판의 개수를 구해준다.
-                    if (min > changeCol) {
-                        min = changeCol;
-                    }
                 }
 
-
+                result1 = Math.min(result1,temp);
 
             }
         }
 
-        System.out.println(min);
+        for(int row=0; row <= c - size; row++){
+            for(int col=0; col <= r - size; col++){
+
+                int temp= 0;
+                for(int i = row; i < row + size; i++){
+                    for(int j=col; j < col + size; j++){
+                        if(i%2 == 0){
+                            if(j%2==0){
+                                if(chess[j][i]!='B'){
+                                    temp+=1;
+                                }
+                            }else{
+                                if(chess[j][i]!='W'){
+                                    temp+=1;
+                                }
+                            }
+
+                        }else{
+                            if(j%2==0){
+                                if(chess[j][i]!='W'){
+                                    temp+=1;
+                                }
+                            }else{
+                                if(chess[j][i]!='B'){
+                                    temp+=1;
+                                }
+                            }
+
+                        }
+                    }
+                }
+                result2 = Math.min(result2,temp);
+
+            }
+        }
+
+
+        //System.out.println(Arrays.deepToString(chess));
+        //System.out.println(result1);
+        //System.out.println(result2);
+
+        int answer = Math.min(result1, result2);
+        System.out.println(answer);
+
 
     }
-
 }
-
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-//BBBBBBBB
-
-
-//BBBWBWBW
-//BBBBWBWB
-//BBBWBWBW
-//BBBBWBWB
-//BBBWBWBW
-//BBBBWBWB
-//BBBWBWBW
-//BBBBWBWB
