7e5ecd6c838d4e5efd6d55c5410466f5189eef1f
the-sleepless-coder <54315562+the-sleepless-coder@users.noreply.github.com>
2025-12-15 21:39:35 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16424 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,49 +1,56 @@
+import java.io.*;
+import java.util.StringTokenizer;
 import java.util.*;
 
 public class Main {
-    // 1 2 3 4 5 7
-    static int[] arr;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int S = sc.nextInt();
+    // M(1<= M <= 10,000,000)
+    // 2개의 합으로 갑옷 만들어짐.
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int target = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
-        for(int i=0; i<N; i++){
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int idx=0; idx<N; idx++){
+            arr[idx] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int result = findArmorSet(S);
-        System.out.println(result);
-    }
+        // 1, 2, 3, 4, 5, 7
 
-    // 투 포인터를 이용해서, 몇개의 갑옷 조합을 만들 수 있는지 확인한다.
-    static int findArmorSet(int S){
-        int s = 0;
-        int e = arr.length - 1;
-        // 갑옷을 몇 벌 만들 수 있는 지 확인한다.
-        int cnt = 0;
+        // 오름차순 기준 정렬된 배열
+        // 숫자가 더 크니까 줄여야 한다.
+        // arr[left] + arr[right] > target
+        // right -=1
+        // 숫자가 더 작으니까 늘려준다.
+        // arr[left] + arr[right] < target
+        // left +=1
+        // 같다면 하나는 늘리고 하나는 줄여서,
+        // target과 동일한 값이 나올지 검증.
 
-        // s와 e는 겹치면 안되기 때문에, s.equals(e)일 때 while문을 종료한다.
-        while(s < e){
-            if(arr[s]+ arr[e] == S){
-                cnt++;
-                s++;
-                e--;
+        // 두 인덱스가 서로 같아서는 안되기 때문에,
+        // 같을 때는 while문을 탈출하게 해야 한다.
+        int left = 0;
+        int right = N-1;
+        int count = 0;
+        while(left < right){
+            int sum = arr[left] + arr[right];
+
+            if(sum > target){
+                right-=1;
+            }else if(sum < target){
+                left+=1;
+            }else{
+                count++;
+                left ++;
+                right --;
             }
-            // 갑옷이 주어진 합보다 작다면, s의 인덱스를 늘려준다.
-            else if(arr[s] + arr[e] < S){
-                s++;
-            }
-            // 갑옷이 주어진 합보다 크다면, e의 인덱스를 줄여준다.
-            else if(arr[s] + arr[e] > S){
-                e--;
-            }
+
         }
 
-
-        return cnt;
+        System.out.println(count);
     }
+
 }
