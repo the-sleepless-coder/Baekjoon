431a9341cd49c4bddeb7af8549d1b2599385a2dc
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-02-24 19:09:36 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,56 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[N];
-        for(int i=0; i<N; i++){
-            arr[i] = br.readLine();
-        }
+        int groupWordCount = 0;
+        for(int test=0; test<N; test++){
+            // 알파벳 그룹의 개수가 1개를 넘어가면,
+            // isGroupWord = false이다.
+            char[] charArr = br.readLine().toCharArray();
+            int[] alphaGroup = new int[26];
 
-        int totCount = 0;
-        String[] result = new String[N];
-        ArrayList<String> arrList = new ArrayList<>();
-        for(int i=0; i<N; i++){
-            char[] charArr = arr[i].toCharArray();
+            boolean isGroupWord = true;
             int len = charArr.length;
+            for(int k=0; k <= len-2; k++){
+                if(k<=len-2){
+                    char curr = charArr[k];
+                    char next = charArr[k+1];
+                    int currIdx = curr-'a';
+                    int nextIdx = next-'a';
 
-            // HashMap에 특정 문자와 가장 마지막으로 들어간 인덱스를 넣는다.
-            // 인덱스가 한번만 들어가거나 가장 마지막으로 넣은 인덱스와의 차이가 1 일 때만 groupchecker = true를 만족시키게 한다.
-            boolean groupchecker = true;
-            HashMap<Character, Integer> map = new HashMap<>();
-            for(int j = 0; j < len; j++){
-                if ( !map.containsKey(charArr[j]) ){
-                    map.put(charArr[j], j);
-                }else if(map.containsKey(charArr[j])){
-                    if( Math.abs(map.get(charArr[j]) - j) > 1 ){
-                        groupchecker = false;
+                    if(curr!=next){
+                        alphaGroup[currIdx]++;
+
+                        // 항상 문자의 끝에서 그룹을 확인하기 때문에,
+                        // 가장 마지막에 문자열이 하나 남아 있다면 그것은 따로 확인이 필요하다.
+                        if(k==len-2){
+                            alphaGroup[nextIdx]++;
+                        }
                     }else{
-                        map.replace(charArr[j], j);
+                        // 서로 다른 알파벳 그룹은 같을 때만 더해지기 때문에,
+                        // 같은데 마지막에 있는 그룹이 있다면 처리를 따로 해줘야 한다.
+                        if(k==len-2){
+                            alphaGroup[nextIdx]++;
+                        }
+
                     }
+
                 }
             }
 
-
-            if(groupchecker){
-                result[i] = arr[i];
-                totCount++;
+            // 알파벳 그룹이 2개이상 나오면,
+            // isGroupWord = false 처리를 해준다.
+            for(int idx=0; idx<26; idx++){
+                if(alphaGroup[idx]>=2){
+                    isGroupWord = false;
+                    break;
+                }
             }
 
+            // System.out.println(Arrays.toString(alphaGroup));
+            if(isGroupWord){
+                groupWordCount++;
+            }
 
         }
 
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(Arrays.toString(result));
-        System.out.println(totCount);
-
-
+        System.out.println(groupWordCount);
 
     }
 }
+
+//hhaappya
+//babb
+//aabba
