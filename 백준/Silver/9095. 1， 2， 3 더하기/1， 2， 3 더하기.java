b016e7e839a25a4a559c19a5489b3b41c1a4aa4d
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-02-27 09:24:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -10,9 +10,20 @@ public class Main {
         for(int t=0; t<testCase; t++){
 
             int N = Integer.parseInt(br.readLine());
-
-            int result = calNum(N);
-
+            
+            int result = -1;
+            if(N==1){result =1;}
+            else if(N==2){result = 2;}
+            else if(N==3){result = 4;}
+            else if(N==4){result = 7;}
+            else if(N==5){result = 13;}
+            else if(N==6){result = 24;}
+            else if(N==7){result = 44;}
+            else if(N==8){result = 81;}
+            else if(N==9){result = 149;}
+            else if(N==10){result = 274;}
+            else if(N==11){result = 504;}
+                
             sb.append(result);
 
             if(t!=testCase-1)
@@ -23,39 +34,4 @@ public class Main {
         System.out.println(sb);
 
     }
-
-    // 변수 초기화는 함수 밖에서,
-    // 선언은 함수 안에서 하는 것이 깔끔한가?
-    static int calNum(int N){
-        if(N==1) return 1;
-        if(N==2) return 2;
-        if(N==3) return 4;
-
-        // 1-idx기준.
-        int[] dp = new int[N+1];
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
-
-        for(int idx=4; idx<=N; idx++){
-            dp[idx] = dp[idx-1]+dp[idx-2]+dp[idx-3];
-        }
-
-        return dp[N];
-    }
-
-}
-
-// dp
-// dp[1] = 1
-// dp[2] = 1+1, 2
-// dp[3] = 1+1+1 | 1+2, 2+1 |  3
-// dp[4] = 1+1+1+1| 2+1+1, 1+2+1, 1+1+2 | 2+2 | 3+1, 1+3
-// dp[5] = 1+1+1+1+1 | 2+1+1+1, 1+2+1+1, 1+1+2+1, 1+1+1+2 | 2+2+1, 2+1+2, 1+2+2 | 3+1+1 *3 | 3+2
-// dp[6] = 1+1+1+1+1+1 | 2+1+1+1+1, ...                   | 2+2+1+1, 2+1+2+1, 2+1+1+2, 1+2+1+2, 1+1+2+2 |
-
-//            1로만           2+1 로만                        2로만       1,3으로만   2,3으로만  3으로만  1, 2, 3으로
-
-// 그러니까 dp[i] = dp[i-1]+dp
-// dp[i-1]에서 계산한 값 +1, dp[i-2]에서 계산한 값 +2, dp[i-3]에서 계산한 값 +3
-// 그래서 3개의 개수를 더하면 dp[i]가 된다.
+}    
