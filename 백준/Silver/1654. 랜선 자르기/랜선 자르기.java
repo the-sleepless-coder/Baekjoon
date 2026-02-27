a00ce209c89add5242d42424c8e671014c952909
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-02-27 21:36:46 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 15400 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,62 +1,61 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 영식이 갖고 있는 랜선의 개수 K개, 필요한 랜선의 개수 N개
-    static int[] arr;
-    static int K, N;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        K = sc.nextInt();
-        N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        arr = new int[K];
-        long max = Integer.MIN_VALUE;
-        for(int i=0; i < K; i++){
-            arr[i] = sc.nextInt();
-            max = Math.max(arr[i], max);
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] lanArr = new int[k];
+        for(int idx=0; idx<k ; idx++){
+            int lan = Integer.parseInt(br.readLine());
+            lanArr[idx] = lan;
         }
 
+        long lanLength = binarySearch(m, lanArr);
 
-        long result = binary(max);
-
-        System.out.println(result);
+        System.out.println(lanLength);
 
     }
 
+    // 랜선의 길이 중 가장 긴 것을 구한다.
+    static long binarySearch(int m, int[] lanArr){
+        int maxLan = -1;
+        int len = lanArr.length;
+        for(int idx=0; idx<len; idx++){
+            maxLan = Math.max(lanArr[idx], maxLan);
+        }
 
-    static long binary(long max){
-        long l = 1;
-        long r = max;
-        long result = 0;
+        long min = 1;
+        long max = maxLan;
 
-        while( l <= r ){
-            long mid = (l+r)/2;
-
-            int sum = 0;
-            for(int i=0; i < K; i++){
-                sum += arr[i] / mid;
+        while(min<=max){
+            int count = 0;
+            long mid = (min+max)/2;
+            for(int idx=0; idx<len; idx++){
+                count += lanArr[idx]/mid;
             }
 
-            // 잘라진 랜선의 개수가, 주어진 N개 보다 많다면 해당 숫자를 반환한다.
-            // 잘라진 랜선의 개수가 주어진 N개보다 많다면, 랜선을 최대한 길게 만들어서 개수를 줄인다.
-            if(N <= sum) {
-                result = mid;
-                l = mid + 1;
+            // 랜선 길이가 너무 길면 앞쪽 반을 탐색
+            if( count < m ){
+                max = mid-1;
             }
-/*
-            else if( N < sum ){
-                l = mid + 1;
-            }
-*/
-            // 잘라진 랜선의 개수가 주어진 N개보다 적다면, 랜선을 더 짧게 자른다.
-            else if(N > sum){
-                r = mid - 1;
+            // 랜선 길이가 너무 짧으면 뒷쪽 반을 탐색
+            // count==m일 때
+            else if( count>=m ) {
+                min = mid + 1;
             }
 
         }
 
-        return result;
+
+        return max;
 
     }
 
 }
+
+// N개의 랜선을 만들 수 있는 랜선의 최대 길이를 출력한다.
