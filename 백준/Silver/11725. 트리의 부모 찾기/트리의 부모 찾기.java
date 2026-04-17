4ed8170fe4465de6c9f431c7d0c410f131fed871
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-04-18 01:17:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 552 ms, Memory: 69408 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,162 +1,75 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 루트에서부터 아래로 내려오면서 부모를 찾는 방식.
         int N = Integer.parseInt(br.readLine());
 
-        // 1-idx기준.
+
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for(int n=0; n<=N; n++){
             graph.add(new ArrayList<>());
         }
 
-        // 어차피 노드 간 연결 관계만 표현하면 되기 때문에,
-        // 노드를 굳이 안 만들어줘도 된다.
+        // 노드 간 관계 형성.
         for(int n=0; n<N-1; n++){
-            String input = br.readLine();
-            String[] strArr = input.split(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int node1 = Integer.parseInt(strArr[0]);
-            int node2 = Integer.parseInt(strArr[1]);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-            graph.get(node1).add(node2);
-            graph.get(node2).add(node1);
-
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
-        // System.out.println(graph);
-
+        // 1-idx 기준.
         boolean[] visited = new boolean[N+1];
-        int startingNum = 1;
-        Map<Integer, ChildrenParent> result = new HashMap<>();
-        // bfs(graph, visited, startingNum, result);
+        int[] parents = new int[N+1];
 
-        // 1-idx기준.
-        int[] parentArr = new int[N+1];
-        bfsFixed(graph, visited, startingNum, parentArr);
-        /**
-        for(Map<Integer, Integer> map: result){
-            for(int key: map.keySet()){
-                sb.append(key).append(" ").append(map.get(key)).append("\n");
-            }
-        }
-         */
+        // bfs방식으로 루트에서 노드까지,
+        // 방문한 순서대로 부모-자식 관계를 형성한다.
+        int root = 1;
+        bfs(graph, root, visited, parents);
 
         StringBuilder sb = new StringBuilder();
-        for(int child=2; child<=N; child++){
-            //ChildrenParent cp = result.get(child);
-            //sb.append(cp.parent);
+        for(int n=2; n<=N; n++){
+            sb.append(parents[n]);
 
-            int parent =  parentArr[child];
-            sb.append(parent);
-            if(child!=N) sb.append("\n");
+            if(n!=N) sb.append("\n");
+
         }
 
         System.out.println(sb);
+
     }
 
-    // 양방향 그래프이기 때문에,
-    // 방문하지 않는 것들만 현재 노드의 자식 노드 그룹으로 넣어준다.
-    static void bfs(ArrayList<ArrayList<Integer>>graph, boolean[] visited, int startingNum, Map<Integer, ChildrenParent> result){
 
+    static void bfs(ArrayList<ArrayList<Integer>> graph, int start, boolean[] visited, int[] parents){
         Queue<Integer> q = new LinkedList<>();
-        q.add(startingNum);
-        visited[startingNum]= true;
+
+        q.add(start);
+        visited[start] = true;
 
         while(!q.isEmpty()){
 
             int curr = q.poll();
 
-            // 아예 꺼내기 편하게,
-            // 자식, 부모 관계를 Map에 넣고 그것을 result배열에 넣어준다.
-            Node node = new Node(curr);
             ArrayList<Integer> neighbors = graph.get(curr);
-            ArrayList<Integer> children = node.children;
+
+            // parents배열에 자식에 대한 부모를 기록해둔다.
             for(int neigh:neighbors){
                 if(!visited[neigh]){
                     q.add(neigh);
                     visited[neigh] = true;
-                    // children.add(neigh);
 
-                    ChildrenParent cp = new ChildrenParent(neigh, curr);
-                    result.put(neigh, cp);
+                    parents[neigh] = curr;
                 }
             }
-
-
         }
-
-    }
-
-    // 최적화 코드
-    // 굳이 새로운 클래스를 만들 필요가 없다.
-    // parentArr에 넣어서 처리하면 훨씬 더 코드가 단순해진다.
-    // 이래서 자료구조가 정말 중요하다.
-    // 무엇에 데이터를 담아서 처리하냐에 따라 시간 복잡도가 아니라,
-    // 새로운 데이터를 생성하는 데 드는 시간과 메모리를 둘다 아낄 수 있으니까 말이야.
-    static void bfsFixed(ArrayList<ArrayList<Integer>>graph, boolean[] visited, int startingNum, int[] parentArr){
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(startingNum);
-        visited[startingNum]= true;
-
-        while(!q.isEmpty()){
-
-            int curr = q.poll();
-
-            // 아예 꺼내기 편하게,
-            // 자식, 부모 관계를 Map에 넣고 그것을 result배열에 넣어준다.
-            Node node = new Node(curr);
-            ArrayList<Integer> neighbors = graph.get(curr);
-            ArrayList<Integer> children = node.children;
-            for(int neigh:neighbors){
-                if(!visited[neigh]){
-                    q.add(neigh);
-                    visited[neigh] = true;
-                    // children.add(neigh);
-
-                    parentArr[neigh] = curr;
-                }
-            }
-
-
-        }
-
 
 
     }
-
-    static class ChildrenParent{
-        int children;
-        int parent;
-
-        public ChildrenParent(int children, int parent){
-            this.children = children;
-            this.parent = parent;
-        }
-
-    }
-
-    // ArrayList를 무조건 초기화해줘야 값을 넣을 수 있다.
-    static class Node{
-        int val;
-        ArrayList<Integer> children ;
-
-        public Node(int val){
-            this.val = val;
-            this.children = new ArrayList<>();
-        }
-
-        public String toString(){
-            return String.format("%d", val);
-        }
-
-    }
-
 }
-
-// 각 노드의 부모를 구하여라.
-// 1번 루트 노드에서부터 아래노드까지 트리를 탐색한다.
-// 위에서부터 인접한 노드를 탐색하면서, 탐색하지 않게 되는 것이 자식 노드가 된다.
