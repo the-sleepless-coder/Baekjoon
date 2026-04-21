32c7e9b0ef6aacd4a8386160880aeb8658c3b57b
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-04-21 16:05:12 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16828 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        //1-idx기준.
+        int[] dp = new int[N+1];
+
+        // 테뷸레이션 방식으로 작은값에서 큰 값을 채워나가는 방식으로 푼다.
+        for(int n=2; n<=N; n++){
+            // 2,3으로 나눴을 때 값, n-1로 의 값 중 더 작은 값이 dp[n] 값이 된다.
+            int min = dp[n-1]+1;
+
+            if(n%2==0) min = Math.min( min , dp[n/2] + 1);
+
+            if(n%3==0) min = Math.min( min , dp[n/3] + 1);
+
+            dp[n] = min;
+        }
+
+        // System.out.println(Arrays.toString(dp));
+        System.out.println(dp[N]);
+
+
+
+    }
+
+
+    // 이분탐색, 슬라이딩 윈도우, 투포인터
+    // 빡센 구현
+}
