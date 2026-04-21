068b6f006be4e1d4bbc374d55fefd32adbae9e52
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-04-21 22:31:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11864 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,52 +4,50 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] numArr = new int[N];
-        for(int idx=0; idx<N; idx++){
-            int num = Integer.parseInt(st.nextToken());
-            numArr[idx] = num;
+        int[] arr = new int[N];
+        for(int n=0; n<N; n++){
+            arr[n]=Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = countLIS(numArr);
-        // System.out.println(Arrays.toString(dp));
-        
-        int LISCount = -1;
-        for(int idx=0; idx<N; idx++){
-            LISCount = Math.max(LISCount, dp[idx]);
-        }
-        System.out.println(LISCount);
-
-    }
-
-    // dp로 가장 긴 증가하는 수열을 찾는다.
-    static int[] countLIS(int[] numArr){
-        int len = numArr.length;
-
-        int[] dp = new int[len];
-        for(int idx=0; idx<len; idx++){
-            dp[idx]=1;
+        int[] dp = new int[N];
+        for(int i=0; i<N; i++){
+            dp[i]=1;
         }
 
-        // 자기 자신의 뒤에 자기 자신 보다 큰 수가 있다면 +1을 해주고,
-        // 해당 값과 기존에 있는 값을 비교해서 더 큰 수를 해당 자리에 넣어준다.
-        for(int i=0; i<len; i++){
-
-            for(int j=i+1; j<len; j++){
-                if(numArr[i] < numArr[j]){
-                    dp[j]= Math.max(dp[j] ,dp[i]+1);
+        // 증가하는 수열 중에서 길이가 긴 것을 dp배열에 저장한다.
+        // 각 시작점에서 증가하는 수열을 구하되,
+        // 그 중에서 가장 긴 것을 구한다.
+        for(int i=0; i<N; i++){
+            for(int j=0; j<i; j++){
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[j]+1, dp[i]);
                 }
             }
+            // System.out.println(Arrays.toString(dp));
         }
 
-        return dp;
+        /**
+         for(int i=0;i<=N-2; i++){
+             for(int j=i+1; j<N; j++){
+                 if(arr[i]<arr[j]){
+                    dp[j] = Math.max(dp[i]+1, dp[j]);
+                 }
+             }
+         }
+         */
 
+        int max = -1;
+        for(int i=0; i<N; i++){
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+        // 가장 긴 증가하는 수열
+        // 3,1,4,2,5,6,7,8
     }
-
-
-
 }
-// DP를 이용한 풀이
-// 현재 위치보다 뒤에 있는 숫자 중, 더 큰 숫자가 있을 때 +1을 기록해주면 된다.
