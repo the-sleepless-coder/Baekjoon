d01aae951c02c0692b286c31de9d4a695fe637e9
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-04-26 03:32:58 +0900

    [Silver III] Title: 두 수의 합, Time: 276 ms, Memory: 33108 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,39 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N =  Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        // 해당 줄에서 읽은 값에 대해서 Token으로 만들고 저장한다.
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
-        HashSet<Integer> s = new HashSet<>();
-
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            s.add(arr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int n=0; n<N; n++){
+            arr[n] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
-        int x = Integer.parseInt(br.readLine());
+        int target = Integer.parseInt(br.readLine());
 
-        //Set안에 x - arr[i]에 해당하는 숫자가 존재하는지 확인하고,
-        // 존재한다면 result +1을 해준다.
-        int result = 0;
-        for(int i=0; i<N; i++){
-            if(s.contains(x - arr[i])){
-                result+=1;
+        int count = 0;
+        int startIdx = 0;
+        int endIdx = N-1;
+
+        // 서로 다른 두 포인터 이기 때문에,
+        // 인덱스가 달라야 한다.
+        // 시간 복잡도 O(N)
+        while(startIdx < endIdx){
+            // target 보다 작다면 작은 포인터를 늘려준다.
+            if(arr[startIdx]+arr[endIdx] < target){
+                startIdx++;
+            }else if(arr[startIdx]+arr[endIdx] > target ){
+                endIdx--;
+            }else{
+                startIdx++;
+                endIdx--;
+                count++;
+                // 여기에서 추가로 뭔가를 해줘야될 것 같은데
             }
+
         }
 
-        result = result/2;
+        System.out.println(count);
 
-        System.out.println(result);
+
 
     }
+    // arr가 정렬된 상태일 때,
+    // start, end 인덱스 2개의 포인터를 통해 구하고자 하는 숫자 합 쌍의 개수를 구한다.
+    // 1 2 3 5 7 9 10 11 12
+
+    // BJ 1940
 }
