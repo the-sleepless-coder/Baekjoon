9fd2aac918cad3126651daf1ddecdc3acb15f65a
이재석 <54315562+the-sleepless-coder@users.noreply.github.com>
2026-06-05 21:45:29 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 7.77 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,72 +1,107 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int n, int width, int num) {
-        // 총 택배 상자 개수 n
-        // 가로로 놓는 상자 개수 w
-        // 꺼내려는 상자 num
+        // 왼쪽 -> 오른쪽 
+        // 오른쪽 -> 왼쪽
         
-        // 풀려는 문제를 한번에 제대로 분석하고,
-        // 그 뒤에 오류 없이 구현하는 편이 훨씬 더 구현 속도가 빠르다.
+        // 꺼내려는 상자 번호가 주어질 때, 
+        // 꺼내려는 상자를 포함해 몇개의 상자를 꺼내야하는지 확인한다. 
         
-        // 요구사항 정확하게 파악
-        // 오류 없이 구현
-        // 시간 복잡도 최적화.
-        
-        // 처음에 한번 제대로 코드를 설계하고 한번에 제대로 구현하는 것이,
-        // 훨씬 더 정확하게 빠르게 코드를 구현하는 데 도움이 된다.
-        int W = width;
-        int H = n/width+1;
-        
-        int[][] graph = new int[H][W];
-        
-        int count=1;
-        outer:for(int h=0; h<H; h++){
-            for(int w=0; w<W; w++){
-                if(count==n+1) break outer;
-                
-                if(h%2==0){
-                    graph[h][w]=count;
-                    
-                }else{
-                    graph[h][W-1-w]= count;
-                }
-                count++;    
-            }
+        int rows = 0;
+        if(n%width != 0)
+        {
+            rows = n/width + 1;
+        }
+        else
+        {
+            rows = n/width;
         }
         
-        // System.out.println(Arrays.deepToString(graph));
-        
-        
-        // 택배와 동일한 열에 있는 숫자가 0이라면,
-        // lastFilled false처리.
-        int numH = -1;
-        int numW = -1;
-        boolean lastFilled = true;
-        for(int h=0; h<H; h++){
-            for(int w=0; w<W; w++){
-                // 분기를 따로 구성 안해주면,
-                // if-else처리하면서 걸려버리니 따로 구성해줘야 해.
-                if(graph[h][w]==num){
-                    numH = h+1;
-                    numW = w+1;
-                    System.out.printf("%d %d", numH, numW);
-                }
-                
-               
-            }
+        int left = 0;
+        if(n%width!=0)
+        {
+            left = rows*width - n;
         }
-         // Out of Bounds Error안 뜨게 해줘야 한다.  
-        if(graph[H-1][numW-1]==0) lastFilled = false;
         
         
-        int answer = lastFilled ? H-numH+1: H-numH; 
-        //System.out.println(lastFilled);
-        //System.out.printf("%d %d", H, numH);
-        // int answer = 0;
+        // 찾는 택새 방자 위치 풀이가 너무 더럽다.
+        int numR = num/width;
+        int numC = num - numR * width;
+        if(numC==0) {
+            numR -= 1;
+            numC = width - 1;
+        }
         
-        return answer;
-    
+        int findC = 0;
+        // 찾는 택배 상자 위치 행이 홀수면, 오른쪽에서 부터 위치를 세어준다.
+        if(numR%2!=0)
+        {
+            findC = width - 1 - numC;  
+        }
+        // 찾는 택배 상자 위치 행이 짝수면, 왼쪽에서부터 위치를 세어준다.
+        else
+        {
+            findC = numC - 1;
+        }
+        
+        Set<Integer> cols = new HashSet<>();
+        // 행의 개수가 짝수면, 택배가 비는 왼쪽열부터 Set에 넣어준다.
+        if(rows % 2 == 0)
+        {
+            for(int c=0; c<left; c++)
+            {    
+                cols.add(c);
+            }
+            
+        }
+        // 행의 개수가 홀수면, 택배가 비는 오른쪽 열부터 Set에 넣어준다.
+        else
+        {
+            for(int c=0; c < left; c++)
+            {
+                cols.add(width - 1 - c);
+            }   
+        }
+        
+        // 택배 상자가 비는 위치 열에 있으면 하나를 더 빼주고, 
+        // 그렇지 않으면 row - numR그대로 답을 확정한다.
+        int result = rows - numR;
+        
+        if(cols.contains(findC))
+        {
+            result-=1;
+        }
+        
+        System.out.printf("%d %d\n", rows, numR);
+        System.out.println(cols);
+        System.out.println(findC);
+        
+     // 24 23  22  21  20 19   
+     // 13  14 15  16 17 18    
+     // 12, 11, 10, 9, 8, 7
+        // 1, 2, 3, 4, 5, 6
+        
+        return result;
     }
+    
+    static StringBuilder builder(int[][] boxes)
+    {
+        int R = boxes.length;
+        int C = boxes[0].length;
+        
+        StringBuilder sb = new StringBuilder();
+        for(int r= 0; r<R; r++)
+        {
+            for(int c=0; c<C; c++)
+            {
+                sb.append(boxes[r][c]).append(" ");
+            }
+            sb.append("\n");
+        }
+        
+        return sb;
+    }
+    
+    
 }
